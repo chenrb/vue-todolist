8c05989f93da470bb58e07315c4cfde9b56cbdbd
Rubi <rubi@canway.net>
2018-08-29 11:47:08 +0800

    父组件

--- src/TodoList.vue
+++ src/TodoList.vue
@@ -1,10 +1,23 @@
 <template>
   <div id="app">
+    <input v-model="inputValue"/>
+    <button @click="handleSubmit">提交</button>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      inputValue: '',
+      list: []
+    }
+  },
+  methods: {
+    handleSubmit: function () {
+      this.list.push(this.inputValue)
+    }
+  }
 }
 </script>
 
