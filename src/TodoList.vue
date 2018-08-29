949fd0ff89701c7cc760af28b340c216e137f5c2
Rubi <rubi@canway.net>
2018-08-29 12:13:35 +0800

    子组件通信

--- src/TodoList.vue
+++ src/TodoList.vue
@@ -1,12 +1,26 @@
 <template>
-  <div id="app">
+  <div>
     <input v-model="inputValue"/>
     <button @click="handleSubmit">提交</button>
+    <ul>
+      <todo-item
+        v-for="(item, index) of list"
+        :content="item"
+        :key="index"
+        :index="index"
+        @delete="handleDelete"
+      ></todo-item>
+    </ul>
   </div>
 </template>
 
 <script>
+import TodoItem from './components/TodoItem'
+
 export default {
+  components: {
+    'todo-item': TodoItem
+  },
   data () {
     return {
       inputValue: '',
@@ -16,6 +30,10 @@ export default {
   methods: {
     handleSubmit: function () {
       this.list.push(this.inputValue)
+      this.inputValue = ''
+    },
+    handleDelete: function (index) {
+      this.list.splice(index, 1)
     }
   }
 }
